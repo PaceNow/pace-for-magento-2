5653b2e99508b9f39169eab8c7700368acb7464f
Phan Shi Wen <phanshiwen@gmail.com>
2020-11-24 10:20:44 +0800

    ci: 🎡 Fix generate readme script

--- internal/generateReadme.sh
+++ internal/generateReadme.sh
@@ -2,4 +2,8 @@ TAG=$1
 README_FILE=./README.md
 cp internal/README.template.md $README_FILE
 
-sed -i '' -e "s/\${tag}/$TAG/" $README_FILE
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e "s/\${tag}/$TAG/" $README_FILE
+else
+  sed -i -e "s/\${tag}/$TAG/" $README_FILE
+fi
