0c986751ea20e32916a83b9be8958275fbd539ea
Lê Văn Kỳ Thịnh <kythinhle@gmail.com>
2021-04-08 14:46:20 +0700

    Update Pace SDK and script location

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
 	map: {
 		'*': {
 			pacePay: "https://pay.pacenow.co/pace-pay.js",
-    	pacePayPlayground: "https://pay-playground.pacenow.co/pace-pay.js",
+    		pacePayPlayground: "https://pay-playground.pacenow.co/pace-pay.js",
 		}
 	}
 };
