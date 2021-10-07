582c2db41e1bdb672c767ba2e0b529f650e5c7df
Lê Văn Kỳ Thịnh <kythinhle@gmail.com>
2021-10-07 17:19:38 +0700

    Update: clear Pace notice before create new order

--- Model/Observer/PaceSalesOrderPlaceBefore.php
+++ Model/Observer/PaceSalesOrderPlaceBefore.php
@@ -4,20 +4,28 @@ namespace Pace\Pay\Model\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\ManagerInterface as EventManager;
 
 use Magento\Framework\Message\ManagerInterface as MessageManagerInterface;
 
 class PaceSalesOrderPlaceBefore implements ObserverInterface
 {
 	/**
+	 * @var Magento\Framework\Event\ManagerInterface
+	 */
+	protected $_eventManager;
+
+	/**
 	 * @var Magento\Framework\Message\ManagerInterface
 	 */
 	protected $_messageManager;
 
     public function __construct(
+    	EventManager $eventManager,
     	MessageManagerInterface $messageManager
     )
     {
+    	$this->_eventManager = $eventManager;
         $this->_messageManager = $messageManager;
     }
 
@@ -26,6 +34,8 @@ class PaceSalesOrderPlaceBefore implements ObserverInterface
     	// Magento\Framework\Message\ManagerInterface
         $message = $this->_messageManager->createMessage( 'notice', 'pace-notice' )->setText( '' );
         $this->_messageManager->addMessage( $message, 'pace' );
+
+        $this->_eventManager->dispatch( 'pace_pay_verifytransaction_before_redirect' );
     }
 
     /**
