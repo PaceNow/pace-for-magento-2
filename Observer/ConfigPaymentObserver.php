be1e149799248df38237fc388549eba4838a9e21
kane-nguyen-goldenowl <107826122+kane-nguyen-goldenowl@users.noreply.github.com>
2022-10-13 16:40:07 +0700

    fix: [PI-607] check store scope before refresh payment plan (#42)
    Co-authored-by: Kane Nguyen <kane.nguyen.globalowl@gmail.com>

--- Observer/ConfigPaymentObserver.php
+++ Observer/ConfigPaymentObserver.php
@@ -4,6 +4,7 @@ namespace Pace\Pay\Observer;
 
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Store\Model\ScopeInterface;
 use Pace\Pay\Controller\Adminhtml\System\Config\RefreshPaymentPlans;
 
 class ConfigPaymentObserver implements ObserverInterface {
@@ -17,6 +18,32 @@ class ConfigPaymentObserver implements ObserverInterface {
 	}
 
 	public function execute(EventObserver $observer) {
-		@$this->refreshPaymentPlans->execute();
+		$scopeData = $this->getScopeData($observer);
+		if ($scopeData['scope'] == ScopeInterface::SCOPE_STORES) {
+			@$this->refreshPaymentPlans->execute();
+		}
 	}
+
+    protected function getScopeData($observer)
+    {
+        $scopeData = [];
+
+        $scopeData['scope']    = 'default';
+        $scopeData['scope_id'] = null;
+
+        $website = $observer->getWebsite();
+        $store   = $observer->getStore();
+
+        if ($website) {
+             $scopeData['scope']    = ScopeInterface::SCOPE_WEBSITES;
+             $scopeData['scope_id'] = $website;
+        }
+
+        if ($store) {
+             $scopeData['scope']    = ScopeInterface::SCOPE_STORES;
+             $scopeData['scope_id'] = $store;
+        }
+
+        return $scopeData;
+    }
 }
